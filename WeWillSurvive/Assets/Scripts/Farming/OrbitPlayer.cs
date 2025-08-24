c159fef9236291a8fe20cbae733f5787cbd737ca
NewbeCat <catonamat098@gmail.com>
2025-08-25 01:03:15 +0900

    Player Animation + Basic Bullet

--- WeWillSurvive/Assets/Scripts/Farming/OrbitPlayer.cs
+++ WeWillSurvive/Assets/Scripts/Farming/OrbitPlayer.cs
@@ -20,12 +20,21 @@ namespace WeWillSurvive
         private bool isCharging = false;
         private float chargeTime = 0f;
 
+        public float pushThreshold = 3f;
+
         private Rigidbody2D rb;
+        private Animator animator;
         private Image circleRenderer;
+        private Bullet currentBullet;
+        public GameObject bulletPrefab;
+
+        public Color circleColor = Color.white;
+        public Color endColor = Color.red;
 
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
             circleRenderer = circle.GetComponent<Image>();
 
             circle.localPosition = new Vector3(radius, 0f, 0f);
@@ -42,22 +51,26 @@ namespace WeWillSurvive
 
             circle.RotateAround(transform.position, Vector3.forward, currentRotationSpeed * Time.deltaTime);
 
-            if (Input.GetMouseButtonDown(0))
+            if (!isCharging && Input.GetMouseButtonDown(0))
             {
                 isCharging = true;
                 chargeTime = 0f;
                 targetRotationSpeed = 0f;
-            }
 
-            if (isCharging && Input.GetMouseButton(0))
+                GameObject bulletObj = Instantiate(bulletPrefab, circle.position, Quaternion.identity, circle);
+                currentBullet = bulletObj.GetComponent<Bullet>();
+            }
+            else if (currentBullet != null && isCharging && Input.GetMouseButton(0))
             {
                 chargeTime += Time.deltaTime;
                 float clampedRatio = Mathf.Clamp01(chargeTime / maxChargeTime);
                 circle.localScale = Vector3.one * (1f + (clampedRatio / 100f));
-                circleRenderer.color = Color.Lerp(Color.white, Color.red, clampedRatio);
-            }
+                circleRenderer.color = Color.Lerp(circleColor, endColor, clampedRatio);
 
-            if (isCharging && Input.GetMouseButtonUp(0))
+                float clampedTime = Mathf.Min(chargeTime, maxChargeTime);
+                currentBullet.Swell(clampedTime / maxChargeTime);
+            }
+            else if (currentBullet != null && isCharging && Input.GetMouseButtonUp(0))
             {
                 isCharging = false;
                 targetRotationSpeed = rotationSpeed;
@@ -69,9 +82,18 @@ namespace WeWillSurvive
                 rb.AddForce(pushDir * (chargeRatio * maxForce), ForceMode2D.Impulse);
 
                 circle.localScale = Vector3.one;
-                circleRenderer.color = Color.white;
+                circleRenderer.color = circleColor;
                 chargeTime = 0f;
+
+                currentBullet.gameObject.transform.SetParent(transform.parent, worldPositionStays: true);
+                currentBullet.Shoot(-pushDir);
+                currentBullet = null;
             }
+
+            animator.SetBool("isPush", (rb.linearVelocity.magnitude > pushThreshold));
+            if (rb.linearVelocity.x < -0.01f) { transform.localRotation = Quaternion.Euler(0f, 180f, 0f); }
+            else if (rb.linearVelocity.x > 0.01f) { transform.localRotation = Quaternion.Euler(0f, 0f, 0f); }
+
         }
     }
 }
