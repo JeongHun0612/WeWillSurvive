2975de299f9a673d5aa6fd25e34df6ddbc990f2a
JeongHun0612 <56680804+JeongHun0612@users.noreply.github.com>
2025-09-15 15:30:48 +0900

    RationEndingPanel 수정

--- WeWillSurvive/Assets/Scripts/UI/Popup/Projecter/RationPanel/RationEndingPanel.cs
+++ WeWillSurvive/Assets/Scripts/UI/Popup/Projecter/RationPanel/RationEndingPanel.cs
@@ -21,7 +21,7 @@ namespace WeWillSurvive
         {
             _dayCountText.text = $"생존 기간 : {GameManager.Instance.Day}일";
             _usedFoodText.text = $"소모한 우주 식량 : {ItemManager.UsedFoodCount:F2}개";
-            _usedWaterText.text = $"소모한 물 : {ItemManager.UsedFoodCount:F2}개";
+            _usedWaterText.text = $"소모한 물 : {ItemManager.UsedWaterCount:F2}개";
             _expedtionCountText.text = $"수행한 탐사 횟수 : {ExpeditionManager.Instance.TotalExpeditionCount}회";
             _characterEventText.text = $"수행한 캐릭터 이벤트 횟수 : {GameEventManager.Instance.CharacterEventPicker.TotalCompleteCount}회";
         }
